b808ee1e9edcd6e4f165654b35799bff9241c48d
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2026-01-20 17:21:42 +0700

    refactor: update adverb resource methods and add helper functions
    # CHANGES
    
    - Fix position filtering to use AdverbPos within Adverbs method.
    - Add LexicalEntries method for converting Adverbs to LexicalEntries type.
    - Implement LexicalEntry method to cast Adverb to LexicalEntry type.
    - Define String method for Adverb returning the lemma written form.
    - Refactor filtering logic to utilize new LexicalEntries conversion helper

--- adverbs.go
+++ adverbs.go
@@ -10,18 +10,33 @@ type Adverb LexicalEntry
 type Adverbs LexicalEntries
 
 func (resource *LexicalResource) Adverbs() (adverbs Adverbs) {
-	lexicalEntries, _ := resource.filterByPos(NounPos)
-	adverbs = Adverbs(lexicalEntries)
+	entries := Adverbs(
+		LexicalEntries(resource.Lexicon.LexicalEntries).
+			filterByPos(AdverbPos),
+	)
 
-	return
+	return entries
 }
 
 func (adverbs Adverbs) filteredByLexFile(kind AdverbKind) (
 	filteredAdverbs Adverbs,
 ) {
-	lexicalEntries := adverbs.filteredByLexFile(kind)
+	lexicalEntries := adverbs.LexicalEntries().
+		filterByLexFile(string(kind))
 	filteredAdverbs = Adverbs(lexicalEntries)
 	return
+}
+
+func (adverbs Adverbs) LexicalEntries() LexicalEntries {
+	return LexicalEntries(adverbs)
+}
+
+func (adverb Adverb) LexicalEntry() LexicalEntry {
+	return LexicalEntry(adverb)
+}
+
+func (adverb Adverb) String() string {
+	return adverb.Lemma.WrittenForm
 }
 
 func (adverbs Adverbs) All() Adverbs {
