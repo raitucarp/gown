58fa002c99eab6acec36080f9c9ad455a4d363f2
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2026-01-21 19:26:13 +0700

    feat: add adverb and adjective filtering and refactor collocation logic
    # CHANGES
    
    - Replace manual string checks with IsCollocation and IsWord methods.
    - Update noun and verb filtering to use abstracted helper methods.
    - Implement Words and Collocations filtering methods for Adverbs type.
    - Add Words and Collocations filtering support for Adjectives type.
    - Remove the unused strings package import from the kind.go file.

--- kind.go
+++ kind.go
@@ -2,7 +2,6 @@ package gown
 
 import (
 	"slices"
-	"strings"
 )
 
 var collocationSeparator = []rune{' ', '-', '!', '/', '+', ':', ','}
@@ -40,9 +39,7 @@ func (nouns Nouns) Words() (wordNouns Nouns) {
 
 func (nouns Nouns) Collocations() (collocationNouns Nouns) {
 	for _, noun := range nouns {
-		if strings.ContainsFunc(
-			noun.Lemma.WrittenForm,
-			containSeparatedCollocation) {
+		if noun.IsCollocation() {
 			collocationNouns = append(collocationNouns, noun)
 		}
 	}
@@ -51,9 +48,7 @@ func (nouns Nouns) Collocations() (collocationNouns Nouns) {
 
 func (verbs Verbs) Words() (wordVerbs Verbs) {
 	for _, verb := range verbs {
-		if !strings.ContainsFunc(
-			verb.Lemma.WrittenForm,
-			containSeparatedCollocation) {
+		if verb.IsWord() {
 			wordVerbs = append(wordVerbs, verb)
 		}
 	}
@@ -62,10 +57,44 @@ func (verbs Verbs) Words() (wordVerbs Verbs) {
 
 func (verbs Verbs) Collocations() (collocationVerbs Verbs) {
 	for _, verb := range verbs {
-		if strings.ContainsFunc(
-			verb.Lemma.WrittenForm,
-			containSeparatedCollocation) {
+		if verb.IsCollocation() {
 			collocationVerbs = append(collocationVerbs, verb)
+		}
+	}
+	return
+}
+
+func (adverbs Adverbs) Words() (wordAdverbs Adverbs) {
+	for _, adverb := range adverbs {
+		if adverb.IsWord() {
+			wordAdverbs = append(wordAdverbs, adverb)
+		}
+	}
+	return
+}
+
+func (adverbs Adverbs) Collocations() (collocationAdverbs Adverbs) {
+	for _, adverb := range adverbs {
+		if adverb.IsCollocation() {
+			collocationAdverbs = append(collocationAdverbs, adverb)
+		}
+	}
+	return
+}
+
+func (adjectives Adjectives) Words() (wordAdjectives Adjectives) {
+	for _, adjective := range adjectives {
+		if adjective.IsWord() {
+			wordAdjectives = append(wordAdjectives, adjective)
+		}
+	}
+	return
+}
+
+func (adjectives Adjectives) Collocations() (collocationAdjectives Adjectives) {
+	for _, adjective := range adjectives {
+		if adjective.IsCollocation() {
+			collocationAdjectives = append(collocationAdjectives, adjective)
 		}
 	}
 	return
